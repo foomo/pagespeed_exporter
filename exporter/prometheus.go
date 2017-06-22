20dd7495f51eac8c4d6771282315183cef86e4a5
Stefan Martinov <stefan.martinov@gmail.com>
2017-06-22 15:13:33 +0200

    Add exporter for all pagespeed metrics with target

---
+++ exporter/prometheus.go
@@ -0,0 +1,127 @@
+package exporter
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/foomo/pagespeed_exporter/googleapi"
+)
+
+var (
+	pageSpeedSpeedScore = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_rulegroup_speed_score",
+		Help: "Pagespeed speed rulegroup rating for the target",
+	}, []string{"target"})
+
+	pageSpeedUsabilityScore = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_rulegroup_usability_score",
+		Help: "Pagespeed usability rulegroup rating for the target",
+	}, []string{"target"})
+
+	pageSpeedCssResponseBytes = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_pagestats_css_response_bytes",
+		Help: "Number of uncompressed response bytes for CSS resources on the page",
+	}, []string{"target"})
+
+	pageSpeedFlashResponseBytes = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_pagestats_flash_response_bytes",
+		Help: "Number of response bytes for flash resources on the page",
+	}, []string{"target"})
+
+	pageSpeedHtmlResponseBytes = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_pagestats_html_response_bytes",
+		Help: "Number of uncompressed response bytes for the main HTML document and all iframes on the page",
+	}, []string{"target"})
+
+	pageSpeedImageResponseBytes = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_pagestats_image_response_bytes",
+		Help: "Number of response bytes for image resources on on the page",
+	}, []string{"target"})
+
+	pageSpeedJavascriptResponseBytes = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_pagestats_javascript_response_bytes",
+		Help: "Number of uncompressed response bytes for JS resources on on the page",
+	}, []string{"target"})
+
+	pageSpeedOtherResponseBytes = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_pagestats_other_response_bytes",
+		Help: "Number of response bytes for other resources on the page",
+	}, []string{"target"})
+
+	pageSpeedTextResponseBytes = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_pagestats_text_response_bytes",
+		Help: "Number of uncompressed response bytes for text resources not covered by other statistics (i.e non-HTML, non-script, non-CSS resources) on the page",
+	}, []string{"target"})
+
+	pageSpeedTotalResponseBytes = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_pagestats_total_response_bytes",
+		Help: "Total size of all request bytes sent by the page",
+	}, []string{"target"})
+
+	pageSpeedCssResourceCount = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_pagestats_css_resource_count",
+		Help: "Number of CSS resources referenced by the page",
+	}, []string{"target"})
+
+	pageSpeedHostCount = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_pagestats_host_count",
+		Help: "Number of unique hosts referenced by the page",
+	}, []string{"target"})
+
+	pageSpeedJsResourceCount = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_pagestats_js_resource_count",
+		Help: "Number of JavaScript resources referenced by the page",
+	}, []string{"target"})
+
+	pageSpeedHttpResourceCount = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_pagestats_http_resource_count",
+		Help: "Number of HTTP resources loaded by the page",
+	}, []string{"target"})
+
+	pageSpeedStaticResourceCount = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pagespeed_pagestats_static_resource_count",
+		Help: " Number of static (i.e. cacheable) resources on the page",
+	}, []string{"target"})
+)
+
+func init() {
+	// Metrics have to be registered to be exposed:
+	prometheus.MustRegister(pageSpeedSpeedScore)
+	prometheus.MustRegister(pageSpeedUsabilityScore)
+	prometheus.MustRegister(pageSpeedCssResponseBytes)
+	prometheus.MustRegister(pageSpeedFlashResponseBytes)
+	prometheus.MustRegister(pageSpeedHtmlResponseBytes)
+	prometheus.MustRegister(pageSpeedImageResponseBytes)
+	prometheus.MustRegister(pageSpeedJavascriptResponseBytes)
+	prometheus.MustRegister(pageSpeedOtherResponseBytes)
+	prometheus.MustRegister(pageSpeedTextResponseBytes)
+	prometheus.MustRegister(pageSpeedTotalResponseBytes)
+	prometheus.MustRegister(pageSpeedCssResourceCount)
+	prometheus.MustRegister(pageSpeedHostCount)
+	prometheus.MustRegister(pageSpeedJsResourceCount)
+	prometheus.MustRegister(pageSpeedHttpResourceCount)
+	prometheus.MustRegister(pageSpeedStaticResourceCount)
+}
+
+func PrometheusMetricsListener(result *googleapi.Result) error {
+
+	if _, ok := result.RuleGroups["SPEED"]; ok {
+		pageSpeedSpeedScore.WithLabelValues(result.Target).Set(float64(result.RuleGroups["SPEED"].Score))
+	}
+	if _, ok := result.RuleGroups["USABILITY"]; ok {
+		pageSpeedUsabilityScore.WithLabelValues(result.Target).Set(float64(result.RuleGroups["USABILITY"].Score))
+	}
+	pageSpeedCssResponseBytes.WithLabelValues(result.Target).Set(float64(result.PageStats.CssResponseBytes))
+	pageSpeedFlashResponseBytes.WithLabelValues(result.Target).Set(float64(result.PageStats.FlashResponseBytes))
+	pageSpeedHtmlResponseBytes.WithLabelValues(result.Target).Set(float64(result.PageStats.HtmlResponseBytes))
+	pageSpeedImageResponseBytes.WithLabelValues(result.Target).Set(float64(result.PageStats.ImageResponseBytes))
+	pageSpeedJavascriptResponseBytes.WithLabelValues(result.Target).Set(float64(result.PageStats.JavascriptResponseBytes))
+	pageSpeedOtherResponseBytes.WithLabelValues(result.Target).Set(float64(result.PageStats.OtherResponseBytes))
+	pageSpeedTextResponseBytes.WithLabelValues(result.Target).Set(float64(result.PageStats.TextResponseBytes))
+	pageSpeedTotalResponseBytes.WithLabelValues(result.Target).Set(float64(result.PageStats.TotalRequestBytes))
+	pageSpeedCssResourceCount.WithLabelValues(result.Target).Set(float64(result.PageStats.NumberCssResources))
+	pageSpeedHostCount.WithLabelValues(result.Target).Set(float64(result.PageStats.NumberHosts))
+	pageSpeedJsResourceCount.WithLabelValues(result.Target).Set(float64(result.PageStats.NumberJsResources))
+	pageSpeedHttpResourceCount.WithLabelValues(result.Target).Set(float64(result.PageStats.NumberResources))
+	pageSpeedStaticResourceCount.WithLabelValues(result.Target).Set(float64(result.PageStats.NumberStaticResources))
+
+	return nil
+}
